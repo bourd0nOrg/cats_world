830c7df15d310098b71286681166746fa05301cd
Nikita Aleksandrov <andone74@gmail.com>
2016-04-04 22:26:25 +0300

    -Add travis config -Add SimpleCompileTest.java

---
+++ src/test/java/com/alex/SimpleCompileTest.java
@@ -0,0 +1,12 @@
+package com.alex;
+
+import com.alex.main.MainClass;
+import org.junit.Test;
+
+public class SimpleCompileTest {
+
+    @Test
+    public void testMain() {
+        MainClass.main(new String[]{});
+    }
+}

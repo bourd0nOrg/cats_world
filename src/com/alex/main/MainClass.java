b67f26dd0aa5e5d8cb765e2cd20a224b6e4d9fd2
Gruzdeva Anna <anyagruzdeva@yandex.ru>
2016-04-05 22:14:22 +0300

    Add properties and methods Raccoon to cats world

--- src/com/alex/main/MainClass.java
+++ src/com/alex/main/MainClass.java
@@ -3,6 +3,7 @@ package com.alex.main;
 
 import com.alex.pets.Cat;
 import com.alex.pets.Fish;
+import com.alex.pets.Raccoon;
 import com.alex.pets.Shark;
 
 public class MainClass {
@@ -10,8 +11,11 @@ public class MainClass {
     public static void main(String[] args) {
         Cat pushok = new Cat("Pushok", "Pers");
         Fish ocun = new Fish("ocun");
+        Raccoon froska = new Raccoon("Froska", "Barbados");
         pushok.eat(ocun);
-
+        froska.play();
+        froska.rinsing();
+        froska.steal();
         Fish karas = new Fish("karas");
         pushok.eat(karas);
 

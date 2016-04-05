80bf26b491647fb72c845b58b4f072f71f56f781
Nikita Aleksandrov <andone74@gmail.com>
2016-04-06 00:13:45 +0300

    add fake test

--- src/test/java/com/alex/SimpleCompileTest.java
+++ src/test/java/com/alex/SimpleCompileTest.java
@@ -3,10 +3,17 @@ package com.alex;
 import com.alex.main.MainClass;
 import org.junit.Test;
 
+import static org.junit.Assert.assertTrue;
+
 public class SimpleCompileTest {
 
     @Test
     public void testMain() {
         MainClass.main(new String[]{});
+    }
+
+    @Test
+    public void fakeTest() {
+        assertTrue(false);
     }
 }

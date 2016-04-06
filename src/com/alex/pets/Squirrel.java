60240495396e5484e4e4a1abdb518f68cdcb9c27
Parfenova Nadegda <nadegdaspp@gmail.com>
2016-04-06 10:54:22 +0300

    Add Squirrel to cats_world

--- src/com/alex/pets/Squirrel.java
+++ src/com/alex/pets/Squirrel.java
@@ -1,5 +1,57 @@
 package com.alex.pets;
 
 
-public class Squirrel {
+public class Squirrel extends Pet implements Alive {
+
+    private String name;
+    private String breed;
+    public Squirrel(String someName, String breed) {
+        super();
+        this.name = someName;
+        this.breed = breed;
+    }
+
+    public void tsocaet() {
+        if (isAlive) {
+            System.out.println("Squirrel " + name + " is tsocaet");
+        } else {
+            System.out.println(";(");
+        }
+    }
+
+    public void sleep() {
+        if (isAlive) {
+            System.out.println("Squirrel " + name + " is sleep");
+        } else {
+            System.out.println(";(");
+        }
+    }
+
+    public void eat(Object nut) {
+        if (isAlive) {
+            System.out.println("Squirrel " + name + " eat " + nut);
+        } else {
+            System.out.println(";(");
+        }
+    }
+
+    public void jump() {
+        if (isAlive) {
+            System.out.println("Squirrel " + name + " is jumping");
+        } else {
+            System.out.println(";(");
+        }
+    }
+
+
+    public String getName() {
+        return name;
+    }
+    public String toString() {
+        return "Squirrel " + name;
+    }
+    public String getBreed() {
+        return breed;
+    }
+
 }

eae18758126dbee88684afd2cdd4ad8d7ab48597
Dron74 <avenger225@yahoo.com>
2016-05-25 06:46:11 +0000

    MeatBasketArray

--- src/main/java/com/alex/eat/Meat.java
+++ src/main/java/com/alex/eat/Meat.java
@@ -3,38 +3,28 @@ package com.alex.eat;
 /**
  * Created by Студент on 04.05.2016.
  */
-public class Meat {
-    private Meat[] meats;
-    private int index;
-    private  int size;
+public class Meat implements Weigh {
+    private int weight;
+    private String color;
 
-    public class PitMeatArray(int size){
-        this.meats = new Meat[size];
-        this.size = size;
-        this.index = 0;
-
+    public Meat(int weight, String color) {
+        this.weight = weight;
+        this.color = color;
     }
 
-    public void addMeat(Meat meat) {
-        if (index < size) {
-            this.meats[index] = meat;
-            index = index + 1;
-        } else {
-            System.out.println("Pit is full");
-        }
+    public int getWeight() {
+        return weight;
     }
 
-    public Meat getMeat() {
-        if (index >= 0) {
-            Meat meat = meats[index];
-            index = index - 1;
-            return meat;
-        } else {
-            throw new IllegalStateException("No meat in pit");
-        }
+    public String getColor() {
+        return color;
     }
 
-    public Meat[] getAllMeats() {
-        return meats;
+    @Override
+    public String toString() {
+        return "Meat{" +
+                "weight=" + weight +
+                ", color='" + color + '\'' +
+                '}';
     }
 }

6ec8923470dcf84e01f07988f6fab941cbd3a38a
bourd0n <andone_90@inbox.ru>
2016-06-08 10:14:58 +0300

    Update Pikachu.java

--- src/main/java/com/alex/pets/Pikachu.java
+++ src/main/java/com/alex/pets/Pikachu.java
@@ -1,5 +1,30 @@
 package com.alex.pets;
 
+import com.alex.exceptions.PetIsDeadException;
 
-public class Pikachu {
-}
+public class Pikachu extends Pet implements Alive{
+    private String name;
+    private String breed;
+    public Pikachu(String someName, String breed) {
+        this.name = someName;
+        this.breed = breed;
+    }
+    public void jump() {
+        if (!isAlive())
+            throw new PetIsDeadException(this);
+        System.out.println("Pikachu " + name + " is jumping");
+    }
+    public void lightning () {
+        if (!isAlive())
+            throw new PetIsDeadException(this);
+        System.out.println("Pikachu " + name + " lightning");
+    }
+    public String getName() {
+        return name;
+    }
+    public String getBreath() {
+        return breed;
+    }
+    public String toString() {
+        return "Pikachu " + name;
+    }}
